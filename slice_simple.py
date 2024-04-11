693599cf501c347142ecc1ad7710dc278ae892c0
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-04-11 20:45:32 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,9 +1,9 @@
 def slice_simple():
     texto = "Awesome"
-    lowe = texto.lower()
-    print(lowe[0:3])
-    print(lowe[2:5])
-    print(lowe[0:7])
+    low = texto.lower()
+    print(low[0:3])
+    print(low[2:5])
+    print(low[0:7])
 
 slice_simple()
 
